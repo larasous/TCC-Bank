3ba0ab7454d1dbe5ed575e9a628c7c8db5cf037f
Lara Sousa <sousalara822@gmail.com>
2023-05-24 22:44:01 -0300

    refatorando estrutura

---
+++ js/validate.js
@@ -0,0 +1,13 @@
+
+window.onload = () => 
+{
+    const form = document.querySelector('#form');
+    form.addEventListener('submit', (event) => {
+        
+        const name = document.querySelector('.student-name');
+        const registration = document.querySelector('.registration');
+        const email = document.querySelector('.email');
+        const course = document.querySelector('.course');
+
+    });
+}
